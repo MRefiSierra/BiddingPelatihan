863dc126b20f5a9a82e05d4933c9aabf572447fc
MasCyberz <dimasned20@gmail.com>
2024-07-19 11:37:13 +0700

    instalasi tabler.io

---
+++ resources/views/main-layout/main-layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bid Pelatihan | @yield('title')</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/core@1.0.0-beta17/dist/css/tabler.min.css">
+</head>
+
+<body>
+    @yield('content')
+    <script src="https://cdn.jsdelivr.net/npm/@tabler/core@1.0.0-beta17/dist/js/tabler.min.js"></script>
+
+</body>
+
+</html>
