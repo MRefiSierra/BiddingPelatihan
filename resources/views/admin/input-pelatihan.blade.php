bf70649d5c00636e38e85804253d5f8f306c12c0
MRefiSierra <muhamadrefi328@gmail.com>
2024-07-19 21:57:40 +0700

    Revisi Desain Input Pelatihan

--- resources/views/admin/input-pelatihan.blade.php
+++ resources/views/admin/input-pelatihan.blade.php
@@ -4,59 +4,92 @@
 
 @section('content')
 
-    <div class="page-wrapper">
-        <div class="page-header d-print-none">
-            <div class="container-xl">
-                <div class="row g-2 align-items-center">
-                    <div class="col">
-                        <h1 class="page-title fs-1">
-                            Input Pelatihan
-                        </h1>
+    <div class="page-wrapper align-items-center justify-content-center d-flex vh-100">
+        <div class="row col-xl-6 col-md-8 col-sm-8 col-10" style="margin: 0px">
+            <div class="page-center">
+                <div class="container py-4">
+                    <div class="card w-100">
+                        <div class="card-body shadow">
+                            <p class="fs-1 fw-semibold text-center mb-4">Input Pelatihan</p>
+                            <form action="{{}}" method="POST" autocomplete="off" novalidate="">
+                                @csrf
+                                <div class="mb-3">
+                                    <label class="form-label fs-3">PRL</label>
+                                    <input type="text" class="form-control fs-4" name="PRL"
+                                        placeholder="Masukkan PRL" />
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label fs-3">Nama</label>
+                                    <input type="text" class="form-control" name="Nama"
+                                        placeholder="Masukkan nama pelatihan" />
+                                </div>
+                                <div class="d-flex gap-sm-3">
+                                    <div class="mb-3 col-sm col-6">
+                                        <label class="form-label fs-3">Tanggal Mulai</label>
+                                        <input type="date" class="form-control" name="TanggalMulai"
+                                            placeholder="Input placeholder" />
+                                    </div>
+                                    <div class="mb-3 col-sm col-6">
+                                        <label class="form-label fs-3">Tanggal Akhir</label>
+                                        <input type="date" class="form-control" name="TanggalAkhir"
+                                            placeholder="Input placeholder" />
+                                    </div>
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label fs-3">Kuota Instruktur</label>
+                                    <input type="text" class="form-control" name="Kuota"
+                                        placeholder="Input placeholder" />
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label fs-3">Lokasi</label>
+                                    <input type="text" class="form-control" name="Lokasi"
+                                        placeholder="Input placeholder" />
+                                </div>
+                                <div class="form-footer text-center">
+                                    <button type="submit" class="btn btn-primary w-50 text-center shadow">Submit</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="page-body">
-            <div class="container-xl p-2">
-                <form class="px-5 pt-3">
-                    <div class="mb-3">
-                        <label class="form-label fs-1">PRL</label>
-                        <input type="text" class="form-control" name="PRL" placeholder="Input placeholder" />
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label fs-1">Nama</label>
-                        <input type="text" class="form-control" name="Nama" placeholder="Input placeholder" />
-                    </div>
-                    <div class="d-flex">
-                        <div class="mb-3 me-5">
-                            <label class="form-label fs-1">Tanggal Mulai</label>
-                            <input type="date" class="form-control" name="TanggalMulai"
-                                placeholder="Input placeholder" />
-                        </div>
-                        <div class="mb-3">
-                            <label class="form-label fs-1">Tanggal Akhir</label>
-                            <input type="date" class="form-control" name="TanggalAkhir"
-                                placeholder="Input placeholder" />
-                        </div>
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label fs-1">Kuota Instruktur</label>
-                        <input type="text" class="form-control" name="Kuota" placeholder="Input placeholder" />
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label fs-1">Lokasi</label>
-                        <input type="text" class="form-control" name="Lokasi" placeholder="Input placeholder" />
-                    </div>
-                    <div class="mt-auto text-right">
-                        <button type="submit" name="submit" class="btn btn-primary">Submit</button>
-                    </div>
-
-                </form>
-            </div>
-        </div>
-
     </div>
 
+    {{-- Numpang Debugging --}}
+    {{-- <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var calendarEl = document.getElementById('calendar');
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                initialView: 'dayGridMonth',
+                events: function(fetchInfo, successCallback, failureCallback) {
+                    fetch('/api/pelatihan/' + fetchInfo.start.getFullYear() + '/' + (fetchInfo.start
+                            .getMonth() + 1))
+                        .then(response => response.json())
+                        .then(data => {
+                            let events = data.map(pelatihan => {
+                                return {
+                                    title: pelatihan.name,
+                                    start: pelatihan.start_date,
+                                    end: pelatihan.end_date,
+                                    color: pelatihan.quota_instruktur > pelatihan
+                                        .selected_instruktur_count ? 'green' : 'red'
+                                };
+                            });
+                            successCallback(events);
+                        });
+                }
+            });
+            calendar.render();
+        });
+    </script>
+    <div class="page-wrapper">
+        <div class="page-body">
+            <div class="container-xl">
+                <div id='calendar'></div>
+            </div>
+        </div>
+    </div> --}}
 
 @endsection
