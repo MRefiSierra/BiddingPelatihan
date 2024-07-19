e53b2a00860feb03d780326fdf5a66d91d81d674
MRefiSierra <muhamadrefi328@gmail.com>
2024-07-19 14:54:46 +0700

    password fitur

--- resources/views/signin.blade.php
+++ resources/views/signin.blade.php
@@ -34,15 +34,32 @@
                                 Password
                             </label>
                             <div class="input-group input-group-flat">
-                                <input type="password" class="form-control" placeholder="Your password"
+                                <input id="password" type="password" class="form-control" placeholder="Your password"
                                     autocomplete="off">
                                 <span class="input-group-text">
                                     <a href="#" class="link-secondary" data-bs-toggle="tooltip"
                                         aria-label="Show password"
                                         data-bs-original-title="Show password"><!-- Download SVG icon from http://tabler-icons.io/i/eye -->
-                                        <i class="ti ti-eye"></i>
-                                        <i class="ti ti-eye-off"></i>
-                                    </a>
+                                        <svg id="eyeopen" class="fs-3" xmlns="http://www.w3.org/2000/svg" width="24"
+                                            height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                            stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                            class="icon icon-tabler icons-tabler-outline icon-tabler-eye">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                            <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+                                            <path
+                                                d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
+                                        </svg>
+                                        <svg class="visually-hidden" id="eyeclosed" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                            stroke-linecap="round" stroke-linejoin="round"
+                                            class="icon icon-tabler icons-tabler-outline icon-tabler-eye-closed">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                            <path d="M21 9c-2.4 2.667 -5.4 4 -9 4c-3.6 0 -6.6 -1.333 -9 -4" />
+                                            <path d="M3 15l2.5 -3.8" />
+                                            <path d="M21 14.976l-2.492 -3.776" />
+                                            <path d="M9 17l.5 -4" />
+                                            <path d="M15 17l-.5 -4" />
+                                        </svg>
                                 </span>
                             </div>
                         </div>
@@ -59,7 +76,20 @@
     <script src="https://cdn.jsdelivr.net/npm/@tabler/core@1.0.0-beta17/dist/js/tabler.min.js"></script>
 </body>
 <script>
+    const eyeclosed = document.getElementById('eyeclosed')
+    const eyeopen = document.getElementById('eyeopen')
+    const password = document.getElementById('password')
 
+    eyeopen.addEventListener('click', function() {
+        password.setAttribute('type', 'text')
+        eyeopen.classList.add('visually-hidden')
+        eyeclosed.classList.remove('visually-hidden')
+    })
+    eyeclosed.addEventListener('click', function() {
+        password.setAttribute('type', 'password')
+        eyeclosed.classList.add('visually-hidden')
+        eyeopen.classList.remove('visually-hidden')
+    })
 </script>
 
 </html>
